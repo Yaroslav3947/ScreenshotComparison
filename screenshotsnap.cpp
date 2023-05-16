d82888a0bbe732f95391386ca2e5c28abf21a4cc
Yaroslav <kogutyaroslav3947@gmail.com>
2023-05-16 21:43:03 +0300

    first commit

---
+++ screenshotsnap.cpp
@@ -0,0 +1,7 @@
+#include "screenshotsnap.h"
+
+
+ScreenshotSnap::ScreenshotSnap() {
+
+}
+
---
+++ screenshotsnap.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <QObject>
+
+
+class ScreenshotSnap : public QObject {
+    Q_OBJECT
+public:
+    ScreenshotSnap();
+};
+
